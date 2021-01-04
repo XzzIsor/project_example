f2ec3e0172f4bf3b67533d6889fd7f59ee0014be
Esteban Ruiz <jesruiz@unicauca.edu.co>
2021-01-03 19:00:26 -0500

    update0

---
+++ android/app/src/main/kotlin/com/example/project_name_uwu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_name_uwu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Menú Principal uwu'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
